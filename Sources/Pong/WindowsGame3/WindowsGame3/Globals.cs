ef1ed6934051a80e7a69c95ac3f41a313194cadb
Jason Hennequin <jasonhennequin@gmail.com>
2012-12-10 21:11:37 +0100

    Pong mis en ligne

---
+++ Sources/Pong/WindowsGame3/WindowsGame3/Globals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace Pong
+{
+    //Classe contenant les variables globales
+    class Globals
+    {
+        public static int largeurFenetre { get; set; }
+
+        public static int hauteurFenetre { get; set; }
+
+        public static SpriteFont police { get; set; }
+
+        public static KeyboardState keyboardState { get; set; }
+
+        public static Random Random { get; set; }
+    }
+}
